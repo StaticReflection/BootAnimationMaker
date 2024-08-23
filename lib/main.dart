2e4a53bf2b1d6713047aae2ad9a7c3a8fb1ea469
StaticReflection <deng3616888303@gmail.com>
2024-08-24 00:27:08 +0800

    feat(i18n): add internationalization support

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   runApp(const MainApp());
@@ -11,6 +12,8 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       darkTheme: ThemeData.dark(),
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
       home: const Scaffold(
         body: Center(
           child: Text('Hello World!'),
