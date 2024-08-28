c4306bc1f6f77952aeb095826a05d2a1ccb1ccfb
StaticReflection <deng3616888303@gmail.com>
2024-08-28 23:38:52 +0800

    fix(ui): fix property configuration card length on Magisk module generation page

--- lib/app/modules/home/pages/magisk_module/page.dart
+++ lib/app/modules/home/pages/magisk_module/page.dart
@@ -6,6 +6,10 @@ class MagiskModulePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MagiskModulePropertiesWidget();
+    return const Column(
+      children: [
+        MagiskModulePropertiesWidget(),
+      ],
+    );
   }
 }
